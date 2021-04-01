da5ca8c2d5c38b11d055d6774be92d40c5ad875d
wuling31715 <wuling31715@gmail.com>
2021-04-01 21:43:34 +0800

    .

--- 12678/main.c
+++ 12678/main.c
@@ -1,39 +1,32 @@
-#include <stdio.h> 
-
-int countDigitOne(int n) {    
-  if (n <= 0) {
-    return 0;
-  }
-  if (n < 10) {
-    return 1;
-  }
-  long ctx = 10;
-  int ctx_count = 1;
-  while (n >= (ctx * 10)) {
-    ctx_count = ctx_count * 10 + ctx;
-    ctx *= 10;
-  }
-  int count = ctx_count;
-  if (n >= (2 * ctx)) {
-    count += count * 2 + count * (n / ctx - 2) + ctx - count;
-    int low_n = countDigitOne(n % ctx);
-    count += low_n;
-  } else {
-    int low_n = countDigitOne(n - ctx);
-    count += low_n * 2 + (n - (ctx - 1)) - low_n;
-  }
-  return count;
-}
+#include <stdio.h>
 
 int main(void) {
-    int t, a, b;
+    int t, a, b, n;
+    n = 1000000;
+    int array[n + 1];
+    int prefix[n + 1];
+    for (int i = 0; i < n + 1; i++) {
+        int count = 0;
+        int r = 0;
+        int j = i;
+        while (j != 0) {
+            r = j % 10;
+            if (r == 1) {
+                count += 1;
+            }
+            j /= 10;
+        }
+        array[i] = count;
+    }
+    prefix[0] = 0;
+    for (int i = 1; i < n + 1; i++) {
+        prefix[i] = prefix[i - 1] + array[i];
+    }
     scanf("%d\n", &t);
     for (int i = 0; i < t; i++) {
         int count = 0;
         scanf("%d %d\n", &a, &b);
-        int MIN = countDigitOne(a - 1);
-        int MAX = countDigitOne(b);
-        count += (MAX - MIN);
-        printf("%d\n", count);
+        printf("%d\n", prefix[b] - prefix[a - 1]);
     }
+    return 0;
 }
