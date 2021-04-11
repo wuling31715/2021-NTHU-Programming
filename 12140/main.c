196c73ccfb26365ee5d13885075954716882dfc7
wuling31715 <wuling31715@gmail.com>
2021-04-12 02:10:33 +0800

    .

--- 12140/main.c
+++ 12140/main.c
@@ -1,72 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void prefixSuffixArray(char* pat, int M, int* pps) {
-    int length = 0;
-    pps[0] = 0;
-    int i = 1;
-    while (i < M) {
-        if (pat[i] == pat[length]) {
-            length++;
-            pps[i] = length;
-            i++;
-        } else {
-            if (length != 0) {
-                length = pps[length - 1];    
-            } else {
-                pps[i] = 0;
-                i++;
-            }
-        }
-    }
-}
-
-void KMPAlgorithm(char* text, char* pattern) {
-    int N = strlen(text); 
-    int M = strlen(pattern) - 1; // remove \n
-    int pps[M];
-    prefixSuffixArray(pattern, M, pps);
-    int i = 0;
-    int j = 0;
+int main(void)
+{
     int count = 0;
-    while (i < N) {
-        if (pattern[j] == text[i]) {
-            j++;
-            i++;
+    int i, j = 0;
+    char str[100], str1[20];
+    printf("Enter the string:\n");
+    scanf(" %[^\n]s", str);
+    printf("Enter the substring to be matched:\n");
+    scanf(" %[^\n]s", str1);
+    int k = strlen(str1);
+    int n = strlen(str);
+    for (i = 0; i < n; i++)
+    {
+        while (str[i] == str[j])
+        {
+            j ++;
         }
-        if (j == M) {
-            count++;
-            // printf("%d\n", i - j);
-            j = pps[j - 1];
-        } else if (i < N && pattern[j] != text[i]) {
-            if (j != 0) {
-                j = pps[j - 1];
-            } else {
-                i = i + 1;
-            }
+        if (j == k)
+        {
+            count ++;
+            j = 0;
         }
-    }
+    } 
     printf("%d\n", count);
-}
-
-int main(void) {
-    char S[1000];
-    char s[1000];
-    // scanf("%[a-z]", S);
-    // scanf("%[a-z]", s);
-    fgets(S, sizeof(S), stdin);
-    fgets(s, sizeof(s), stdin);    
-    int t;
-    scanf("%d\n", &t);
-    for (int i = 0; i < t; i++) {
-        int l, r;
-        scanf("%d %d\n", &l, &r);
-        int size = r - l + 1;
-        char subS[size];
-        for (int j = 0; j < size; j++) {
-            subS[j] = S[j + l - 1];
-        }
-        KMPAlgorithm(subS, s);
-    }
     return 0;
 }
