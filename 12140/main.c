8acf2b17545f5ec7112a7f4887b94c83de98c1cd
wuling31715 <wuling31715@gmail.com>
2021-04-12 20:29:13 +0800

    .

--- 12140/main.c
+++ 12140/main.c
@@ -18,11 +18,7 @@ void string_matching(int N, int M) {
            count++;
            j = 0;
         }
-        prefix[i + 1] = count; // start from 1
-    }
-    int tmp = N - M + 1;
-    for (int i = tmp; i <= N; i++) {
-        prefix[i] = prefix[tmp];
+        prefix[i + M] = count; // store at the end of matching
     }
 }
 
@@ -32,8 +28,21 @@ int main(void) {
     int N = strlen(text);
     int M = strlen(pattern);
     string_matching(N, M);
-    for (int i = 1; i <= N; i++) {
-        printf("%d ", prefix[i]);
+    // for (int i = 0; i <= N; i++) {
+    //     printf("%c ", text[i]);
+    // }
+    // printf("\n");
+    // for (int i = 1; i <= N; i++) {
+    //     printf("%d ", prefix[i]);
+    // }
+    // printf("\n");
+    int t, l, r, frequence;
+    scanf("%d\n", &t);
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d\n", &l, &r);
+        frequence = prefix[r] - prefix[l - 1];
+        // printf("%d %d %d\n", l, r, frequence);
+        printf("%d\n", frequence);
     }
     return 0;
 }
