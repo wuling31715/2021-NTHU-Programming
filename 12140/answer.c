df6f360503eb78f3ec98785b51e028bf565c8752
wuling31715 <wuling31715@gmail.com>
2021-04-13 10:14:00 +0800

    .

--- 12140/answer.c
+++ 12140/answer.c
@@ -1,63 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-#define Max(x,y) ((x>y) ? x : y)
-#define Min(x,y) ((x<y) ? x : y)
 
-char S[1001],p[1001];
-int s_len,p_len;
+#define Max(x, y) ((x > y) ? x : y)
+#define Min(x, y) ((x < y) ? x : y)
 
-int check_H(int id)
-{
-    for(int i=id,j=0; j<p_len; j++,i++)
-    {
-        if(i>=s_len || S[i] != p[j]) return 0;
+char S[1001], p[1001];
+int s_len, p_len;
+
+int check_head(int id) {
+    for(int i = id, j = 0; j < p_len; j++,i++) {
+        if (i >= s_len || S[i] != p[j]) {
+            return 0;
+        }
     }
     return 1;
 }
 
-int check_E(int id)
-{
-
-    for(int i=id,j=p_len-1; j>=0; j--,i--)
-    {
-        if(i<0 || S[i] != p[j]) return 0;
+int check_end(int id) {
+    for (int i = id, j = p_len - 1; j >= 0; j--,i--) {
+        if (i < 0 || S[i] != p[j]) {
+            return 0;
+        }
     }
     return 1;
 }
 
-int main ()
-{
-
-    scanf("%s%s",S,p);
-    int pre_H[1002],pre_E[1002];
+int main (void) {
+    scanf("%s%s", S, p);
+    int prefix_head[1002], prefix_end[1002];
     s_len = strlen(S);
     p_len = strlen(p);
-
-    for(int i=0; i<s_len; i++)
-    {
-        pre_H[i+1] = check_H(i) + pre_H[i];
-    }
-    for(int i=0; i<s_len; i++)
-    {
-        pre_E[i+1] = check_E(i) + pre_E[i];
+    for (int i = 0; i < s_len; i++) {
+        prefix_head[i + 1] = check_head(i) + prefix_head[i];
     }
     for (int i = 0; i < s_len; i++) {
-        printf("%d ", pre_H[i]);
+        prefix_end[i + 1] = check_end(i) + prefix_end[i];
     }
-    printf("\n");
-    for (int i = 0; i < s_len; i++) {
-        printf("%d ", pre_E[i]);
-    }
-    printf("\n");
-
     int q;
-    scanf("%d",&q);
+    scanf("%d", &q);
     while(q--)
     {
         int l,r;
-        scanf("%d%d",&l,&r);
-        int ans = pre_E[r] - pre_H[l-1];
-        printf("%d\n",Max(ans,0));
+        scanf("%d%d\n", &l, &r);
+        int ans = prefix_end[r] - prefix_head[l-1];
+        printf("%d\n", Max(ans, 0));
     }
     return 0;
 }
