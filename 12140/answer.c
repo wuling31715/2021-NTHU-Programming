52051b946b83fbee582f68e278fe7c9ac376aff3
wuling31715 <wuling31715@gmail.com>
2021-04-13 11:29:22 +0800

    .

--- 12140/answer.c
+++ 12140/answer.c
@@ -8,7 +8,7 @@ char S[1001], p[1001];
 int s_len, p_len;
 
 int check_head(int id) {
-    for(int i = id, j = 0; j < p_len; j++,i++) {
+    for(int i = id, j = 0; j < p_len; j++, i++) {
         if (i >= s_len || S[i] != p[j]) {
             return 0;
         }
@@ -17,7 +17,7 @@ int check_head(int id) {
 }
 
 int check_end(int id) {
-    for (int i = id, j = p_len - 1; j >= 0; j--,i--) {
+    for (int i = id, j = p_len - 1; j >= 0; j--, i--) {
         if (i < 0 || S[i] != p[j]) {
             return 0;
         }
@@ -28,22 +28,28 @@ int check_end(int id) {
 int main (void) {
     scanf("%s%s", S, p);
     int prefix_head[1002], prefix_end[1002];
+    prefix_head[0] = 0;
+    prefix_end[0] = 1;
     s_len = strlen(S);
     p_len = strlen(p);
     for (int i = 0; i < s_len; i++) {
         prefix_head[i + 1] = check_head(i) + prefix_head[i];
+        // printf("%d ", prefix_head[i]);
     }
+    // printf("\n");
     for (int i = 0; i < s_len; i++) {
         prefix_end[i + 1] = check_end(i) + prefix_end[i];
+        // printf("%d ", prefix_end[i]);
     }
+    // printf("\n");
     int q;
     scanf("%d", &q);
     while(q--)
     {
-        int l,r;
+        int l, r;
         scanf("%d%d\n", &l, &r);
         int ans = prefix_end[r] - prefix_head[l-1];
-        printf("%d\n", Max(ans, 0));
+        printf("%d\n", Max(ans, 0) - 1);
     }
     return 0;
 }
